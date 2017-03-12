c12667b826cb8e203124515312b5a6fc91b5f279
Luke <luke.e.philips@gmail.com>
2017-03-12 14:07:07 -0700

    looked at making teams dynamically advance rounds, decided it was out of proect scope.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   for(var j=1;j<6;j++){
     for (var i = 0; i < 64 / Math.pow(2,j); i++) {
-      $('.round-'+j).append("<div class='row match1 team"+ i % 2+"'><span class='internal-border'>team " + i + "</span></div>")
+      $('.round-'+j).append("<div class='row team"+ i % 2+"'><span class='internal-border'>team " + i + "</span></div>")
     };
   }
   for (var i = 0; i < 2; i++) {
-    $('.round-6').append("<div class='row match1 team"+ i % 2+"'><span class='internal-border'>team " + i + "</span></div>")
+    $('.round-6').append("<div class='row team"+ i % 2+"'><span class='internal-border'>team " + i + "</span></div>")
   };
 });
